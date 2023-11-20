a9a4758a19ff19ae3c3ade6d5fe6471a2883cce0
Toanphuoc2611 <toanphuoc26112003@gmail.com>
2023-11-20 21:09:47 +0700

    commit

---
+++ Lab7/src/lab/Entry.java
@@ -0,0 +1,10 @@
+package lab;
+
+public class Entry {
+	public static void main(String[] args) {
+		GA_NQueenAlgo algo = new GA_NQueenAlgo();
+		Node re = algo.execute();
+		re.displayBoard();
+		System.out.println(re.getH());
+	}
+}
